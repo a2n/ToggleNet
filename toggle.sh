d9ee7b1c7a34b788bcdf55de4f0bb12577558725
Alan Lu <a@28int.com>
2012-05-10 14:55:47 +0800

    First commit.

---
+++ toggle.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+banHost="foo1.example.com,foo2.example.com"
+
+function isRoot() {
+    me=`id -u`
+    root=`id -u root`
+    if [ $me -ne $root ]; then
+	echo root permission required.
+	    return 0
+    else
+	return 1
+    fi
+}
+
+function off() {
+    cmd="ipfw -q add"
+    ks="keep-state"
+    $cmd 1 check-state
+    # Flush all rules
+    ipfw -q flush
+
+    index=1
+    for str in $(echo $banHost | tr "," "\n")
+    do
+	$cmd $index deny all from any to $str out setup $ks
+	let index++
+    done
+
+    echo -n "Added $index host"
+    if [ $index -gt 1 ]; then
+	echo "s"
+    fi
+}
+
+function on() {
+    cmd="ipfw -q delete"
+
+    index=1
+    for str in $(echo $banHost | tr "," "\n")
+    do
+	$cmd $index
+	let index++
+    done
+
+    echo -n "Deleted $index host"
+    if [ $index -gt 1 ]; then
+	echo "s"
+    fi
+}
+
+function firewall() {
+    case $1 in
+	'on' )
+	on
+	;;
+    'off' )
+	off
+	;;
+    esac
+}
+
+isRoot
+if [ $? -eq 0 ]; then
+    exit
+else
+    if [ -z $1 ]; then
+	echo "$0 [on | off]"
+	echo "Toggle network connection ability [on | off]."
+	echo
+	exit
+    else
+	firewall $1
+    fi
+fi
